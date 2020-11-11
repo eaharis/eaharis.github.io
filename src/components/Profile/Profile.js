41c1508492ddf85054c9c350e2a8c0b3835872a1
Jack Buckley <bucklj4@mcmaster.ca>
2020-11-10 21:26:12 -0500

    Fixed some li's not being ListGroup.Item and fixed some style issues.

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -7,12 +7,15 @@ import "./Profile.css"
 
 import McMasterLogo from "./McMaster.png";
 import CalendarIcon from "./calendar.png";
+
+import Tags from "@yaireo/tagify/dist/react.tagify" // React-wrapper file
+import "@yaireo/tagify/dist/tagify.css" // Tagify CSS
 /*
  * dsjljd 
 */
 const Profile = () => {
     return (
-        <Container>
+        <Container className="Profile">
             <script src="bootstrap-slider.min.js"></script>
             <link href="bootstrap-slider.min.css" rel="stylesheet" />
             <Row>
@@ -30,7 +33,7 @@ const Profile = () => {
                                         <Col md={12} className="mb-3">
                                             <Form.Group>
                                                 <Form.Label className="font-weight-bold"><FontAwesomeIcon icon={faPencilAlt} /> Description</Form.Label>
-                                                <Form.Control as="textarea" rows={3} placeholder="Enter a short description about yourself here..."></Form.Control>
+                                                <Form.Control as="textarea" rows={3} placeholder="Enter a short description about yourself here..." style={{ resize: "none" }}></Form.Control>
                                             </Form.Group>
                                         </Col>
                                     </Form.Row>
@@ -56,17 +59,20 @@ const Profile = () => {
                                 <h6 class="mb-0"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="feather feather-twitter mr-2 icon-inline text-info"><path d="M23 3a10.9 10.9 0 0 1-3.14 1.53 4.48 4.48 0 0 0-7.86 3v1A10.66 10.66 0 0 1 3 4s-4 9 5 13a11.64 11.64 0 0 1-7 2c9 5 20 0 20-11.5a4.5 4.5 0 0 0-.08-.83A7.72 7.72 0 0 0 23 3z"></path></svg>Twitter</h6>
                                 <DropdownButton title="@jack" variant="outline-secondary" type="button">
                                     {/* TODO: do something when clicked */}
-                                </DropdownButton>                                </ListGroup.Item>
-                            <li class="list-group-item d-flex justify-content-between align-items-center flex-wrap">
+                                </DropdownButton>
+                            </ListGroup.Item>
+                            <ListGroup.Item className="d-flex justify-content-between align-items-center flex-wrap">
                                 <h6 class="mb-0"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="feather feather-instagram mr-2 icon-inline text-danger"><rect x="2" y="2" width="20" height="20" rx="5" ry="5"></rect><path d="M16 11.37A4 4 0 1 1 12.63 8 4 4 0 0 1 16 11.37z"></path><line x1="17.5" y1="6.5" x2="17.51" y2="6.5"></line></svg>Instagram</h6>
                                 <DropdownButton title="@jack" variant="outline-secondary" type="button">
                                     {/* TODO: do something when clicked */}
-                                </DropdownButton>                                  </li>
-                            <li class="list-group-item d-flex justify-content-between align-items-center flex-wrap">
+                                </DropdownButton>
+                            </ListGroup.Item>
+                            <ListGroup.Item className="d-flex justify-content-between align-items-center flex-wrap">
                                 <h6 class="mb-0"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="feather feather-facebook mr-2 icon-inline text-primary"><path d="M18 2h-3a5 5 0 0 0-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 0 1 1-1h3z"></path></svg>Facebook</h6>
                                 <DropdownButton title="@jack" variant="outline-secondary" type="button">
                                     {/* TODO: do something when clicked */}
-                                </DropdownButton>                                  </li>
+                                </DropdownButton>
+                            </ListGroup.Item>
                         </ListGroup>
                     </Card>
                 </Col>
@@ -143,7 +149,7 @@ const Profile = () => {
                                     <Col md={6}>
                                         <Form.Label>Alert frequency</Form.Label>
                                         {/* TODO: Fix */}
-                                        <input data-slider-id="freq" style={{ width: "90%;" }} class="form-group" id="ex13" type="text" data-slider-ticks="[Daily, Weekly, Biweekly, Monthly]" data-slider-ticks-snap-bounds="30" data-slider-ticks-labels='["$Daily", "$Weekly", "$Biweekly", "$Monthly"]' />
+                                        <input data-slider-id="freq" style={{ width: "90%" }} class="form-group" id="ex13" type="text" data-slider-ticks="[Daily, Weekly, Biweekly, Monthly]" data-slider-ticks-snap-bounds="30" data-slider-ticks-labels='["$Daily", "$Weekly", "$Biweekly", "$Monthly"]' />
                                     </Col>
                                 </Form.Row>
                             </Form.Row>
