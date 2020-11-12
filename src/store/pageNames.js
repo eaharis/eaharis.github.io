78f5e573b1fb1ac6d2272341a923a51020d47af7
Esam Haris <eaharis@Esams-Air.cgocable.net>
2020-11-11 21:12:05 -0500

    Fixed routing issue

--- src/store/pageNames.js
+++ src/store/pageNames.js
@@ -1,7 +1,7 @@
 export const baseURL = "https://eaharis.github.io";
 export const HOME = "/";
-export const BROWSE = "/browse";
-export const PROMOTIONS = "/promotions";
-export const DINEIN = "/dine-in";
-export const STORIES = "/stories";
-export const SIGNIN = '/sign-in';
+export const BROWSE = "/#browse";
+export const PROMOTIONS = "/#promotions";
+export const DINEIN = "/#dine-in";
+export const STORIES = "/#stories";
+export const SIGNIN = '/#sign-in';
