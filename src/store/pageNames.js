d58f24981da184dec99ed7f5cae7eadb3b3a0aaa
Esam Haris <eaharis@Esams-Air.cgocable.net>
2020-11-12 00:07:04 -0500

    updated routes

--- src/store/pageNames.js
+++ src/store/pageNames.js
@@ -1,7 +1,6 @@
-const BASEURL = "";
-export const HOME = BASEURL + "/";
-export const BROWSE = "/browse";
-export const PROMOTIONS = "/promotions";
-export const DINEIN = "/dine-in";
-export const STORIES = "/stories";
-export const SIGNIN = '/sign-in';
+export const HOME = "https://eaharis.github.io/testRepo/#";
+export const BROWSE = HOME + "/browse";
+export const PROMOTIONS = HOME + "/promotions";
+export const DINEIN = HOME + "/dine-in";
+export const STORIES = HOME + "/stories";
+export const SIGNIN = HOME + '/sign-in';
