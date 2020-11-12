b8ca05090062243438e0db553e15570275734d26
Esam Haris <eaharis@Esams-Air.cgocable.net>
2020-11-12 00:16:44 -0500

    updated routes

--- src/store/pageNames.js
+++ src/store/pageNames.js
@@ -1,6 +1,13 @@
-export const HOME = "https://eaharis.github.io/testRepo/#";
-export const BROWSE = HOME + "/browse";
-export const PROMOTIONS = HOME + "/promotions";
-export const DINEIN = HOME + "/dine-in";
-export const STORIES = HOME + "/stories";
-export const SIGNIN = HOME + '/sign-in';
+// export const HOME = "https://eaharis.github.io/testRepo/#";
+// export const BROWSE = HOME + "/browse";
+// export const PROMOTIONS = HOME + "/promotions";
+// export const DINEIN = HOME + "/dine-in";
+// export const STORIES = HOME + "/stories";
+// export const SIGNIN = HOME + '/sign-in';
+
+export const HOME = "testRepo/#/";
+export const BROWSE = HOME + "testRepo/#/browse";
+export const PROMOTIONS = HOME + "testRepo/#/promotions";
+export const DINEIN = HOME + "testRepo/#/dine-in";
+export const STORIES = HOME + "testRepo/#/stories";
+export const SIGNIN = HOME + 'testRepo/#/sign-in';
